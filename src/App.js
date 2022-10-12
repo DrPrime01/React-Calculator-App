53c43da525afc48269868733db520f36af77f1e5
Gabriel Shoyombo <gabrielshoyombo2002@gmail.com>
2022-10-12 12:21:56 +0100

    React-app Calculator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,141 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+import cancel from "./calculator images/cancel.png";
+import calculatorIcon from "./calculator images/calculatorIcon.png";
+import clock from "./calculator images/clock.png";
+import ruler from "./calculator images/ruler.png";
+import { arrOfBtnRows, calculate } from "./btnSeed";
 
 function App() {
+  let rowsOfBtns = arrOfBtnRows.map((rows) => (
+    <div className={rows.className} id={rows.id} key={rows.id}>
+      {rows.children.map((btns) => (
+        <div
+          className={btns.className}
+          id={btns.id}
+          key={btns.id}
+          onClick={handleClick}
+        >
+          {btns.content}
+        </div>
+      ))}
+    </div>
+  ));
+
+  function handleClick(e) {
+    let btn;
+    btn = e.target;
+    const btnIdAttribute = btn.getAttribute("id");
+    if (btnIdAttribute === "delete") {
+      const duplicateCalcInput = calculationInput.split("");
+      duplicateCalcInput.pop();
+      //If the duplicateCalcInput is more than zero, return the remaining value after deletion
+      if (duplicateCalcInput.length > 0) {
+        setCalculationInput(duplicateCalcInput.join(""));
+        // else return zero value
+      } else {
+        setCalculationInput("0");
+      }
+    } else if (btn.classList.contains("clear")) {
+      setCalculationInput("0");
+      setResultInput("");
+    } else if (btn.classList.contains("operator")) {
+      if (
+        !calculationInput.split("").includes("+") &&
+        !calculationInput.split("").includes("-") &&
+        !calculationInput.split("").includes("x") &&
+        !calculationInput.split("").includes("/") &&
+        !calculationInput.split("").includes("%")
+      ) {
+        setCalculationInput(calculationInput + btn.textContent);
+      }
+    } 
+    else if (btn.classList.contains("equals")) {
+      const result = calculate(firstInput, thirdInput, secondInput)
+      setResultInput(result);
+    } else {
+      if (calculationInput === "0") {
+        setCalculationInput(btn.textContent);
+      } else {
+        setCalculationInput(calculationInput + btn.textContent);
+      }
+    }
+  }
+
+  const [calculationInput, setCalculationInput] = useState("0");
+  const [resultInput, setResultInput] = useState("");
+  const [firstInput, setFirstInput] = useState("0");
+  const [secondInput, setSecondInput] = useState("");
+  const [thirdInput, setThirdInput] = useState("0");
+
+  useEffect(() => {
+    const splitCalculationInput = calculationInput.split("");
+    if (
+      splitCalculationInput.includes("+") ||
+      splitCalculationInput.includes("-") ||
+      splitCalculationInput.includes("/") ||
+      splitCalculationInput.includes("x") ||
+      splitCalculationInput.includes("%")
+    ) {
+      let indexOfOperator = splitCalculationInput.findIndex(
+        (arr) =>
+          arr === "-" ||
+          arr === "+" ||
+          arr === "/" ||
+          arr === "%" ||
+          arr === "x"
+      );
+      let arrBeforeOperator = splitCalculationInput.slice(0, indexOfOperator);
+      let beforeJoinedArray = arrBeforeOperator.join("");
+      setFirstInput(beforeJoinedArray);
+      setSecondInput(splitCalculationInput[indexOfOperator]);
+      let neededIndex = indexOfOperator + 1;
+      let slicedArray = splitCalculationInput.slice(neededIndex);
+      let joinedSlicedArray = slicedArray.join("");
+      setThirdInput(joinedSlicedArray);
+      console.log(firstInput, secondInput, thirdInput);
+    } else {
+      setCalculationInput(calculationInput);
+    }
+  }, [calculationInput, firstInput, secondInput, thirdInput]);
+
+  // useEffect(() => {
+  //   setArrOfInputs([firstInput, secondInput, thirdInput]);
+  // }, [thirdInput, secondInput, firstInput]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex-calc">
+        <div className="calculator" id="flex-calculator">
+          <div className="screen">
+            <span className="inputs" id="input-1">
+              {calculationInput}
+            </span>
+            <span className="inputs" id="input-2">
+              {resultInput}
+            </span>
+            <div className="icons">
+              <span className="row-1">
+                <img src={clock} alt="clock" />
+                <img src={ruler} alt="ruler" />
+                <img src={calculatorIcon} alt="calc" />
+              </span>
+              <span>
+                <img
+                  src={cancel}
+                  alt="cancel"
+                  onClick={handleClick}
+                  className="row-2 btns"
+                  id="delete"
+                />
+              </span>
+            </div>
+          </div>
+          <hr />
+          <div className="buttons">{rowsOfBtns}</div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/btnSeed.js
@@ -0,0 +1,179 @@
+export const arrOfBtnRows = [
+  {
+    id: "row-5",
+    className: "row-5-btn",
+    children: [
+      {
+        id: "clear",
+        className: "btns clear",
+        content: "C",
+      },
+      {
+        id: "brac",
+        className: "btns green-texts operator-id",
+        content: "( )",
+      },
+      {
+        id: "percent",
+        className: "btns green-texts operator",
+        content: "%",
+      },
+      {
+        id: "divide",
+        className: "btns green-texts operator",
+        content: "/",
+      },
+    ],
+  },
+  {
+    id: "row-4",
+    className: "row-4-btn",
+    children: [
+      {
+        id: "7-btn",
+        className: "btns number",
+        content: "7",
+      },
+      {
+        id: "8-btn",
+        className: "btns number",
+        content: "8",
+      },
+      {
+        id: "9-btn",
+        className: "btns number",
+        content: "9",
+      },
+      {
+        id: "times",
+        className: "btns green-texts operator",
+        content: "x",
+      },
+    ],
+  },
+  {
+    id: "row-3",
+    className: "row-3-btn",
+    children: [
+      {
+        id: "4-btn",
+        className: "btns number",
+        content: "4",
+      },
+      {
+        id: "5-btn",
+        className: "btns number",
+        content: "5",
+      },
+      {
+        id: "6-btn",
+        className: "btns number",
+        content: "6",
+      },
+      {
+        id: "minus",
+        className: "btns green-texts operator",
+        content: "-",
+      },
+    ],
+  },
+  {
+    id: "row-2",
+    className: "row-2-btn",
+    children: [
+      {
+        id: "1-btn",
+        className: "btns number",
+        content: "1",
+      },
+      {
+        id: "2-btn",
+        className: "btns number",
+        content: "2",
+      },
+      {
+        id: "3-btn",
+        className: "btns number",
+        content: "3",
+      },
+      {
+        id: "plus",
+        className: "btns green-texts operator",
+        content: "+",
+      },
+    ],
+  },
+  {
+    id: "row-1",
+    className: "row-1-btn",
+    children: [
+      {
+        id: "plus-minus",
+        className: "btns operator",
+        content: "+/-",
+      },
+      {
+        id: "zero",
+        className: "btns number",
+        content: "0",
+      },
+      {
+        id: "point",
+        className: "btns decimal",
+        content: ".",
+      },
+      {
+        id: "equals",
+        className: "btns equals",
+        content: "=",
+      }
+    ],
+  },
+];
+
+export function calculate(input1, input2, operator) {
+  let calculation;
+  switch (operator) {
+    case "+":
+      if (input2) {
+        calculation = parseFloat(input1) + parseFloat(input2);
+      } else {
+        calculation = parseFloat(input1) + 0;
+      }
+      break;
+    case "-":
+      if (input2) {
+        calculation = parseFloat(input1) - parseFloat(input2);
+      } else {
+        calculation = parseFloat(input1) - 0;
+      }
+      break;
+    case "/":
+      if (input2) {
+        calculation = parseFloat(input1) / parseFloat(input2);
+      }
+      if (input2 === "0") {
+        calculation = "Maths Error";
+      } else {
+        calculation = parseFloat(input1) / 1;
+      }
+      break;
+    case "x":
+      if (input2) {
+        calculation = parseFloat(input1) * parseFloat(input2);
+      } else {
+        calculation = parseFloat(input1) * 1;
+      }
+      break;
+    case "%":
+      if (input2) {
+        calculation = (parseFloat(input1) / 100) * parseFloat(input2);
+      } else {
+        calculation = parseFloat(input1) / 100;
+      }
+      break;
+      default: return calculation;
+  }
+  console.log(calculation);
+  return calculation;
+}
